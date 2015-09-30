7bc4c941569c2d43797882618a2757afe7c71bac
Mark Stillwell <mark@stillwell.me>
2015-09-30 23:30:51 +0100

    stupid script mistake...

--- get-ssh-key.sh
+++ get-ssh-key.sh
@@ -8,7 +8,7 @@ TMPFILE=$(mktemp)
 while [ ${ATTEMPTS} -gt 0 ]; do
   ATTEMPTS=$((${ATTEMPTS}-1))
   rm -f ${TMPFILE}
-  [ -f /root/.ssh/authorized_keys ] && cat > ${TMPFILE}
+  [ -f /root/.ssh/authorized_keys ] && cat /root/.ssh/authorized_keys > ${TMPFILE}
   curl -m 5 -s http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key \
     >> ${TMPFILE} 2>/dev/null
   if [ $? -eq 0 ]; then
