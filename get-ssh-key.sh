590946c59f14bad5500dc8670b9989237b10a73f
Mark Stillwell <mark@stillwell.me>
2015-09-30 22:37:43 +0100

    ah, also need new set key logic here...

--- get-ssh-key.sh
+++ get-ssh-key.sh
@@ -5,10 +5,12 @@ mkdir -p /root/.ssh
 chmod 700 /root/.ssh
 
 TMPFILE=$(mktemp)
-while [ ! -f /root/.ssh/authorized_keys ] && [ ${ATTEMPTS} -gt 0 ]; do
+while [ ${ATTEMPTS} -gt 0 ]; do
   ATTEMPTS=$((${ATTEMPTS}-1))
-  curl -sf http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key \
-    > ${TMPFILE} 2>/dev/null
+  rm -f ${TMPFILE}
+  [ -f /root/.ssh/authorized_keys ] && cat > ${TMPFILE}
+  curl -m 5 -s http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key \
+    >> ${TMPFILE} 2>/dev/null
   if [ $? -eq 0 ]; then
     cat ${TMPFILE} >> /root/.ssh/authorized_keys
     chmod 0600 /root/.ssh/authorized_keys
@@ -19,7 +21,7 @@ while [ ! -f /root/.ssh/authorized_keys ] && [ ${ATTEMPTS} -gt 0 ]; do
     cat /root/.ssh/authorized_keys
     echo
     echo "********************************************************"
+    break
   fi
-  sleep 1
 done
 rm -f ${TMPFILE}
